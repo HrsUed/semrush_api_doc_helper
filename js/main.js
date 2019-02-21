6b5dfddd0c9e1a1277d74708d730d941ab14b80a
innova-HrsUed <ueda@innova-jp.com>
2019-02-21 19:08:35 +0900

    変数名の変更

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-var columns = [];
+var reportColumnDescriptions = [];
 
-const toText = (filterd_options) => {
+const toText = (filterdOptions) => {
   let html = '';
-  filterd_options.forEach((opt, idx) => {
+  filterdOptions.forEach((opt, idx) => {
     html += `<p><span class='name'>${opt.name}</span>: <span class='des'>${opt.des}</span></p>`;
   });
 
@@ -11,14 +11,14 @@ const toText = (filterd_options) => {
 
 const setOptions = () => {
   $('#columns tbody tr').each((idx, tr) => {
-    columns.push({
+    reportColumnDescriptions.push({
       'name': $(tr).find('th span').text(),
       'des': $(tr).children('td').text(),
     });
   });
 
   // Kd: Keyword Difficulty Indexは公式にも漏れてる
-  columns.push({
+  reportColumnDescriptions.push({
     'name': 'Kd',
     'des': 'Keyword Difficulty Index',
   });
@@ -31,24 +31,24 @@ $(() => {
     const $td = $(ev.target);
     if ($td.children('span').data('original-title')) return;
 
-    const option_name = $td.parent('tr').children('td').first().text();
-    console.log(option_name);
-    if (option_name != "export_columns" && option_name != "display_filter") return;
+    const optionName = $td.parent('tr').children('td').first().text();
+    console.log(optionName);
+    if (optionName != "export_columns" && optionName != "display_filter") return;
 
-    const option_text = $td.html();
+    const optionText = $td.html();
     $td.empty();
 
     const $span = $('<span></span>');
-    $span.text(option_text);
+    $span.text(optionText);
     $td.append($span);
 
-    option_array = option_text.replace(/ /g, '').split(',');
-    const filterd_options = columns.filter((item, idx) => {
-      if (option_array.indexOf(item.name) >= 0) return true
+    optionArray = optionText.replace(/ /g, '').split(',');
+    const filterdOptions = reportColumnDescriptions.filter((item, idx) => {
+      if (optionArray.indexOf(item.name) >= 0) return true
     });
 
     $span.data('toggle', 'tooltip');
-    $span.attr('title', toText(filterd_options)).tooltip({
+    $span.attr('title', toText(filterdOptions)).tooltip({
       'placement': 'top',
       'html': true,
     });
